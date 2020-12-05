718abd796adead847312dd7f8c641a20df1c7289
asahi <hawaiianlovers58@gmail.com>
2020-12-05 16:32:41 +0900

    add Q456 to HEAD

--- task.rb
+++ task.rb
@@ -28,6 +28,7 @@ def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
+  sports.compact!
 
   # 以下は変更しないで下さい
   p sports
@@ -38,14 +39,15 @@ def q5
   array2 = [1, 5, 8, 10]
 
   # 以下に回答を記載
-
+  p array1.empty?
+  p array2.empty?
 end
 
 def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  p numbers1.map! { |x| x * 10 }
 end
 
 def q7
